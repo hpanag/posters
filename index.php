7c7bf2688c4fa5deaf24bc0342adc06ee7e49989
Harj Panag <harj.panag@gmail.com>
2020-09-21 04:52:45 -0400

    without ai

---
+++ index.php
@@ -0,0 +1,376 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Harjot.com - Posters</title>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+<link href='http://fonts.googleapis.com/css?family=Raleway:400,200,300,600,700,800' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300,200' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.min.css" rel="stylesheet" media="screen">
+<link href="css/responsive.css" rel="stylesheet" media="screen" type="text/css"/>
+<link rel="stylesheet" href="sidr/stylesheets/jquery.sidr.dark.css">
+<script src="js/jquery.min.js"></script>
+<script src="sidr/jquery.sidr.min.js"></script>
+<script type="text/javascript" src="js/smoothscroll.js"></script>
+
+
+<script type='text/javascript' src='//dsms0mj1bbhn4.cloudfront.net/assets/pub/shareaholic.js' data-shr-siteid='8ef951f0b917021c762425dae19e84ee' data-cfasync='false' async='async'></script>
+ 
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-71752002-1', 'auto');
+  ga('send', 'pageview');
+  
+  ga('set', 'userId', {{USER_ID}}); // Set the user ID using signed-in user_id.
+
+</script>
+
+
+</head>
+
+<body>
+	<div class="header">
+    	<div class="container">
+    		<div class="logo-menu">
+        		<div class="logo">
+            		<h1><a href="#">HARJOT</a></h1>
+            	</div>
+                <!--<a id="simple-menu" href="#sidr">Toggle menu</a>-->
+                <div id="mobile-header">
+<a class="responsive-menu-button" href="#"><img src="images/11.png"/></a>
+</div>
+            	<div class="menu" id="navigation">
+            		<ul>
+                    	<li><a href="#">Home</a></li>
+                 
+                        <li><a href="#stories">Posters</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+            	</div>
+        	</div>
+        </div>
+    </div>
+    
+    <div class="banner">
+    	<div class="container">
+        	<div class="header-text">
+           	  <p class="big-text">Feel free to download / print any or all posters FREE of charge.        	</p>
+        	</div>
+        </div>
+    </div>
+    
+    <div class="color-border">
+    </div>
+    
+    <div class="desc">
+    	<div class="container">
+        	<h2><span class="text-head">Translators Needed</span></h2>
+            <p><span class="box-desc">Looking for individuals who can translate these posters into other languages.</span></p>
+        </div>
+    </div>
+    <div class="stories" id="stories">
+    	<div class="container">
+    
+            
+        	<div class="stories-section">
+            	<ul>
+                
+                
+                
+                
+                	<li>
+                    	<a href="poster.php?name=universal_appeal&title=Universal Appeal">
+                    	<div class="story-img"><img src="posters/thumb/universal_appeal.png"/>
+                            
+                        </div>
+                        	<div class="story-box">
+                            	<h4>Universal Appeal</h4>
+                       
+                       
+                       
+                         </div> </a>
+                    
+                    </li>
+   
+   
+                   	<li>
+                    	<a href="poster.php?name=victoria_cross&title=Victoria Cross">
+                    	<div class="story-img"><img src="posters/thumb/victoria_cross.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Victoria Cross</h4>
+                         </div>
+                        </a>
+                    </li>
+
+                	<li>
+                    	<a href="poster.php?name=uniquenessof&title=Uniqueness of Sri Guru Granth Sahib">
+                    	<div class="story-img"><img src="posters/thumb/uniquenessof.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Uniqueness of Sri Guru Granth Sahib</h4>
+                         </div>
+                        </a>
+                    </li>
+
+
+                	<li>
+                    	<a href="poster.php?name=travels_of_guru_nanak&title=Travels of Guru Nanak">
+                    	<div class="story-img"><img src="posters/thumb/travels_of_guru_nanak.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Travels of Guru Nanak</h4>
+                         </div>
+                        </a>
+                    </li>
+
+
+                	<li>
+                    	<a href="poster.php?name=sikhs_in_world_wars&title=Sikhs in World Wars">
+                    	<div class="story-img"><img src="posters/thumb/sikhs_in_world_wars.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Sikhs in world wars</h4>
+                         </div>
+                        </a>
+                    </li>
+
+                	<li>
+                    	<a href="poster.php?name=sikhmarriage&title=Sikh Marriage">
+                    	<div class="story-img"><img src="posters/thumb/sikhmarriage.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Sikh marriage</h4>
+                         </div>
+                        </a>
+                    </li>
+
+
+                	<li>
+                    	<a href="poster.php?name=sikh_women&title=Sikh Women">
+                    	<div class="story-img"><img src="posters/thumb/sikh_women.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Sikh women</h4>
+                         </div>
+                        </a>
+                    </li>
+
+
+                	<li>
+                    	<a href="poster.php?name=sacrifices_made&title=Sacrifices made for India">
+                    	<div class="story-img"><img src="posters/thumb/sacrifices_made.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Sacrifices made for India</h4>
+                         </div>
+                        </a>
+                    </li>
+
+                	<li>
+                    	<a href="poster.php?name=promises_broken&title=Promises Broken">
+                    	<div class="story-img"><img src="posters/thumb/promises_broken.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Promises broken</h4>
+                         </div>
+                        </a>
+                    </li>
+
+
+                	<li>
+                    	<a href="poster.php?name=religious_fasting&title=Religious Fasting">
+                    	<div class="story-img"><img src="posters/thumb/religious_fasting.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Religious fasting</h4>
+                         </div>
+                        </a>
+                    </li>
+
+
+                	<li>
+                    	<a href="poster.php?name=quotes&title=What others have to say">
+                    	<div class="story-img"><img src="posters/thumb/quotes.png"/></div>
+                        	<div class="story-box">
+                            	<h4>What others have to say</h4>
+                         </div>
+                        </a>
+                    </li>
+
+
+                	<li>
+                    	<a href="poster.php?name=more_quotes&title=What others have to say 2">
+                    	<div class="story-img"><img src="posters/thumb/more_quotes.png"/></div>
+                        	<div class="story-box">
+                            	<h4>What others have to say 2</h4>
+                         </div>
+                        </a>
+                    </li>
+
+
+                	<li>
+                    	<a href="poster.php?name=kirtan&title=Kirtan">
+                    	<div class="story-img"><img src="posters/thumb/kirtan.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Kirtan</h4>
+                         </div>
+                        </a>
+                    </li>
+
+                	<li>
+                    	<a href="poster.php?name=khanda&title=The Khanda">
+                    	<div class="story-img"><img src="posters/thumb/khanda.png"/></div>
+                        	<div class="story-box">
+                            	<h4>The Khanda</h4>
+                         </div>
+                        </a>
+                    </li>
+                	<li>
+                    	<a href="poster.php?name=heaven_and_hell&title=Heaven and Hell">
+                    	<div class="story-img"><img src="posters/thumb/heaven_and_hell.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Heaven and hell</h4>
+                         </div>
+                        </a>
+                    </li>
+                	<li>
+                    	<a href="poster.php?name=guru_nanak_the_explorer&title=Guru Nanak the explorer">
+                    	<div class="story-img"><img src="posters/thumb/guru_nanak_the_explorer.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Guru Nanak the explorer</h4>
+                         </div>
+                        </a>
+                    </li>
+                	<li>
+                    	<a href="poster.php?name=gurbani_meant_to_be_applied&title=Gurbani meant to be applied">
+                    	<div class="story-img"><img src="posters/thumb/gurbani_meant_to_be_applied.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Gurbani meant to be applied</h4>
+                         </div>
+                        </a>
+                    </li>
+                	<li>
+                    	<a href="poster.php?name=father_and_son&title=Father and son">
+                    	<div class="story-img"><img src="posters/thumb/father_and_son.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Father and son</h4>
+                         </div>
+                        </a>
+                    </li>
+                	<li>
+                    	<a href="poster.php?name=empty_rituals&title=Empty Rituals">
+                    	<div class="story-img"><img src="posters/thumb/empty_rituals.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Empty rituals</h4>
+                         </div>
+                        </a>
+                    </li>
+                	<li>
+                    	<a href="poster.php?name=children&title=Our children our future">
+                    	<div class="story-img"><img src="posters/thumb/children.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Our children our future</h4>
+                         </div>
+                        </a>
+                    </li>
+                	<li>
+                    	<a href="poster.php?name=bhai_mani_singh&title=Bhai Mani Singh">
+                    	<div class="story-img"><img src="posters/thumb/bhai_mani_singh.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Bhai Mani Singh</h4>
+                         </div>
+                        </a>
+                    </li>
+                	<li>
+                    	<a href="poster.php?name=bhai_kanhaiya&title=Bhai Kanhaiya">
+                    	<div class="story-img"><img src="posters/thumb/bhai_kanhaiya.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Bhai Kanhaiya</h4>
+                         </div>
+                        </a>
+                    </li>
+                	<li>
+                    	<a href="poster.php?name=battle_of_saragarhi&title=Battle of Saragarhi">
+                    	<div class="story-img"><img src="posters/thumb/battle_of_saragarhi.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Battle of Saragarhi</h4>
+                         </div>
+                        </a>
+                    </li>
+                	<li>
+                    	<a href="poster.php?name=a_visitor_from_rome&title=A visitor from Rome">
+                    	<div class="story-img"><img src="posters/thumb/a_visitor_from_rome.png"/></div>
+                        	<div class="story-box">
+                            	<h4>A visitor from Rome</h4>
+                         </div>
+                        </a>
+                    </li>
+                	<li>
+                    	<a href="poster.php?name=3pillars&title=Three pillars of Sikhism">
+                    	<div class="story-img"><img src="posters/thumb/3pillars.png"/></div>
+                        	<div class="story-box">
+                            	<h4>Three pillars of Sikhism</h4>
+                         </div>
+                        </a>
+                    </li>
+                	 
+                 	<li>
+                    	 
+                    	<div class="story-img"> </div>
+                        	<div class="story-box">
+                            	<h4>More coming soon...</h4>
+                         </div>
+                      
+                    </li>  
+   
+   
+                    
+                    
+                </ul>
+            </div>
+        </div>
+    </div>
+   	
+    <div class="contact" id="contact">
+    	<div class="container">
+        	<h3 class="text-head">Contact me </h3>
+            <p class="box-desc">Please feel free to share your comments or suggestions. I can be reached at the following email address.</p>
+               <p class="box-desc"><a href="mailto:harj@panag.ca"><img src="images/email.png" width="200" height="50" alt=""/></a></p>
+            	<div class="contact-section"></div>
+        </div>
+    </div>
+    <div class="color-border">
+    </div>
+    <div class="footer">
+        	<div class="container">
+            	<div class="infooter">
+                <p class="copyright">Copyright &copy; Harjot.com <a class="credit"href="http://www.harjot.com"></a></p>
+            	</div>
+            <ul class="socialmedia">
+                
+                <li><a href="https://www.facebook.com/harjot.panag"><i class="icon-facebook"></i></a></li>
+                
+                <li><a href="http://ca.linkedin.com/in/panag"><i class="icon-linkedin"></i></a></li>
+                 
+            </ul>
+            </div>
+        </div>
+        <script type="text/javascript" src="js/jquery.nicescroll.min.js"></script>
+        <script type="text/javascript">		
+			 $(document).ready(function() {
+				$('#simple-menu').sidr({
+				side: 'right'
+			});
+			});
+			$('.responsive-menu-button').sidr({
+				name: 'sidr-main',
+				source: '#navigation',
+				side: 'right'
+
+				});
+			$(document).ready(
+			function() {
+			$("html").niceScroll({cursorborder:"0px solid #fff",cursorwidth:"5px",scrollspeed:"70"});
+			}
+			);
+		</script>
+</body>
+</html>
